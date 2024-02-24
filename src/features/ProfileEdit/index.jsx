6221918ec92384a21b82f7f27350aaae980a507c
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2024-02-24 17:04:41 +0500

    Profile edit finished.

--- src/features/ProfileEdit/index.jsx
+++ src/features/ProfileEdit/index.jsx
@@ -8,10 +8,7 @@ const ProfileEdit = () => {
   const [image, setImage] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordRepeat, setShowPasswordRepeat] = useState(false);
-  const [passwordvalidation, setPasswordValidation] = useState(false);
-  const [repeatPassword, setRepeatPassword] = useState("");
   const router = useRouter();
-  const [validation, setValidation] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
   const [formData, setFormData] = useState({
     region: "",
@@ -28,21 +25,9 @@ const ProfileEdit = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    let updatedFormData = { ...formData, [name]: value };
-
-    if (name === "phoneNumber") {
-      updatedFormData = {
-        ...updatedFormData,
-        validation: false,
-      };
-    }
-
-    setFormData(updatedFormData);
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handlePasswordChange = (e) => {
-    setRepeatPassword(e.target.value);
-  };
   useEffect(() => {
     const getUserInfo = async () => {
       try {
@@ -70,20 +55,15 @@ const ProfileEdit = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (formData.passwords === repeatPassword) {
-      setPasswordValidation(false);
-      try {
-        const response = await api.put("/user/updateInformation", formData);
-        if (response.status === 200) {
-          const newToken = response.data.obj;
-          updateToken(newToken);
-          window.location.href = "/";
-        }
-      } catch (error) {
-        console.error(error);
+    try {
+      const response = await api.put("/user/updateInformation", formData);
+      if (response.status === 200) {
+        const newToken = response.data.obj;
+        updateToken(newToken);
+        router.push("/");
       }
-    } else {
-      setPasswordValidation(true);
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -107,31 +87,6 @@ const ProfileEdit = () => {
           className={styles.ProfileEditFormInputs}>
           <div
             style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-            {/* <div className={styles.ProfileEditImage}>
-              <label
-                htmlFor="profileImage"
-                className={styles.profileImageUploadButton}>
-                {image ? (
-                  <Image
-                    className={styles.UploadedImage}
-                    src={URL.createObjectURL(image)}
-                    alt="Yangilandi"
-                    width={100}
-                    height={100}
-                  />
-                ) : (
-                  <span>+</span>
-                )}
-                <input
-                  type="file"
-                  id="profileImage"
-                  name="profileImage"
-                  accept="image/*"
-                  onChange={handleImageUpload}
-                />
-              </label>
-            </div> */}
-
             <div className={styles.ProfileEditFormInput}>
               <label htmlFor="region">Viloyat</label>
               <div className={styles.selectWrapper}>
@@ -200,97 +155,44 @@ const ProfileEdit = () => {
             </div>
             <div className={styles.ProfileEditFormInput}>
               <label htmlFor="phone">Telefon Raqam</label>
-              {validation ? (
-                <div>
-                  <input
-                    style={{ border: "1px solid red" }}
-                    type="text"
-                    name="phoneNumber"
-                    placeholder="Telefon raqamingizni kiriting"
-                    value={formData.phoneNumber}
-                    onChange={handleChange}
-                    required
-                  />
-                  {validation && (
-                    <p
-                      style={{ color: "red", margin: "0", fontSize: "0.9rem" }}>
-                      Telefon raqamingizni {`to\'g\'ri`} formatda kiriting
-                      (+998XXXXXXXXX).
-                    </p>
-                  )}
-                </div>
-              ) : (
-                <input
-                  type="text"
-                  name="phoneNumber"
-                  placeholder="Telefon raqamingizni kiriting"
-                  value={formData.phoneNumber}
-                  onChange={handleChange}
-                  required
-                />
-              )}
+              <input
+                type="text"
+                name="phoneNumber"
+                placeholder="Telefon raqamingizni kiriting"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                required
+              />
             </div>
             <div className={styles.ProfileEditFormInput}>
-              <label htmlFor="password">Parol</label>
+              <label htmlFor="passwords">Parol</label>
               <div className={styles.passwordInputContainer}>
-                {passwordvalidation ? (
-                  <input
-                    style={{ border: "1px solid red" }}
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    placeholder="Parol kiriting"
-                    value={formData.passwords}
-                    onChange={handleChange}
-                    autoComplete="new-password"
-                    pattern="(?=.*\d)(?=.*[A-Z]).{8,}"
-                  />
-                ) : (
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    placeholder="Parol kiriting"
-                    value={formData.passwords}
-                    onChange={handleChange}
-                    autoComplete="new-password"
-                    pattern="(?=.*\d)(?=.*[A-Z]).{8,}"
-                  />
-                )}
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="passwords"
+                  placeholder="Parol kiriting"
+                  value={formData.passwords}
+                  onChange={handleChange}
+                  autoComplete="new-password"
+                  required
+                />
                 <div
                   className={styles.eyeIcon}
                   onClick={togglePasswordVisibility}>
                   <i className={`ri-eye-${showPassword ? "off-" : ""}line`}></i>
                 </div>
               </div>
-              {passwordvalidation && (
-                <p style={{ color: "red", margin: "0", fontSize: "0.7rem" }}>
-                  Parol 8 ta belgidan {`ko\'p`}, 1 ta raqam va 1 ta katta harf
-                  kerak.
-                </p>
-              )}
             </div>
             <div className={styles.ProfileEditFormInput}>
               <label htmlFor="passwordRepeat">Parolni takrorlang</label>
               <div className={styles.passwordInputContainer}>
-                {passwordvalidation ? (
-                  <input
-                    style={{ border: "1px solid red" }}
-                    type={showPasswordRepeat ? "text" : "password"}
-                    name="passwordRepeat"
-                    value={repeatPassword}
-                    onChange={handlePasswordChange}
-                    placeholder="Parol takrorlang"
-                    autoComplete="new-password"
-                  />
-                ) : (
-                  <input
-                    type={showPasswordRepeat ? "text" : "password"}
-                    name="passwordRepeat"
-                    value={repeatPassword}
-                    onChange={handlePasswordChange}
-                    placeholder="Parol takrorlang"
-                    autoComplete="new-password"
-                  />
-                )}
+                <input
+                  type={showPasswordRepeat ? "text" : "password"}
+                  name="passwordRepeat"
+                  placeholder="Parol takrorlang"
+                  autoComplete="new-password"
+                  required
+                />
                 <div
                   className={styles.eyeIcon}
                   onClick={togglePasswordVisibilityRepeat}>
@@ -300,11 +202,6 @@ const ProfileEdit = () => {
                     }line`}></i>
                 </div>
               </div>
-              {passwordvalidation && (
-                <p style={{ color: "red", margin: "0", fontSize: "0.7rem" }}>
-                  Parol bir xil emas.
-                </p>
-              )}
             </div>
           </div>
           <button name="submit">Saqlash</button>
